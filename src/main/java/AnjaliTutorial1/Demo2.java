09195fa5a36d029779c93027975d6ea68bb15d2b
prafulp1030 <prafulp1030@gmail.com>
2025-01-22 20:40:50 +0530

    Anjali Automation Test Cases 2

---
+++ src/main/java/AnjaliTutorial1/Demo2.java
@@ -0,0 +1,14 @@
+package AnjaliTutorial1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Anjali Automation Test Cases 2::::: ");
+		System.out.println("Anjali Automation Test Cases 2::::: ");
+		System.out.println("Anjali Automation Test Cases 2::::: ");
+		System.out.println("Anjali Automation Test Cases 2::::: ");
+		System.out.println("Anjali Automation Test Cases 2::::: ");
+		
+	}
+}
