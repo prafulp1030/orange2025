3ceec1b570f56b01626515d17ac73ab9aa8f6d72
prafulp1030 <prafulp1030@gmail.com>
2025-01-22 20:38:08 +0530

    Rahul automation Test Cases

---
+++ src/main/java/RahulTutorial/Demo111.java
@@ -0,0 +1,16 @@
+package RahulTutorial;
+
+public class Demo111 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Rahul automation Test Cases");
+		System.out.println("Rahul automation Test Cases");
+		System.out.println("Rahul automation Test Cases");
+		System.out.println("Rahul automation Test Cases");
+		System.out.println("Rahul automation Test Cases");
+		System.out.println("Rahul automation Test Cases");
+		
+	}
+
+}
