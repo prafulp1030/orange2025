3e574d757a560a67dc9aa344987317b860401b30
prafpawar11 <prafulp1030@gmail.com>
2025-01-22 20:33:56 +0530

    Automation test Enginner new Code

---
+++ src/main/java/AnjaliTutorial1/Demo1.java
@@ -0,0 +1,15 @@
+package AnjaliTutorial1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Anjali Have written Automation test Cases");
+		System.out.println("Anjali Have written Automation test Cases");
+		System.out.println("Anjali Have written Automation test Cases");
+		
+		
+		
+	}
+	
+}
